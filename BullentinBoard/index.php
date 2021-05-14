f36e6269e8e2fe0dd136b6ed25556419e8817b83
roiaki <roitoitoitoi@yahoo.co.jp>
2021-05-14 20:42:13 +0900

    20210514_chapter5

--- BullentinBoard/index.php
+++ BullentinBoard/index.php
@@ -3,11 +3,56 @@ var_dump($_POST);
 // メッセージを保存するファイルのパス設定
 define( 'FILENAME', './message.txt');
 
+// 変数の初期化
+$now_date = null;
+$data = null;
+$file_handle = null;
+$split_data = null;
+$message = array();
+$message_array = array();
+
+// タイムゾーン設定
+date_default_timezone_set('Asia/Tokyo');
+
+if( !empty($_POST['btn_submit']) ) {
+	
+	// 「w」はファイル内容を一旦リセットして書き込みを行い、
+	// 「a」は末端から追記する形で書き込み
+	// ファイルが開けたら　true
+	if( $file_handle = fopen( FILENAME, "a") ) {
+	
+		// 書き込み日時を取得
+		$now_date = date("Y-m-d H:i:s");
+	
+		// 書き込むデータを作成
+		$data = "'".$_POST['view_name']."','".$_POST['message']."','".$now_date."'\n";
+			
+		// 書き込み
+		fwrite( $file_handle, $data);
+
+		// ファイルを閉じる
+		fclose( $file_handle);
+	}	
+}
+
+// [r]:読み込みのみでオープン
+if( $file_handle = fopen( FILENAME,'r') ) {
+    
+	while( $data = fgets($file_handle) ){
+        $split_data = preg_split( '/\'/', $data);
+
+        $message = array(
+            'view_name' => $split_data[1],
+            'message' => $split_data[3],
+            'post_date' => $split_data[5]
+        );
+        array_unshift( $message_array, $message);
+    }
+    // ファイルを閉じる
+    fclose( $file_handle);
+}
+
 ?>
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -34,6 +79,18 @@ define( 'FILENAME', './message.txt');
 <hr>
 <section>
 <!-- ここに投稿されたメッセージを表示 -->
+
+<?php if( !empty($message_array) ): ?>
+<?php foreach( $message_array as $value ): ?>
+<article>
+    <div class="info">
+        <h2><?php echo $value['view_name']; ?></h2>
+        <time><?php echo date('Y年m月d日 H:i', strtotime($value['post_date'])); ?></time>
+    </div>
+    <p><?php echo $value['message']; ?></p>
+</article>
+<?php endforeach; ?>
+<?php endif; ?>
 </section>
 </body>
 </html>
