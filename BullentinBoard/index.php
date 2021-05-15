876c0e08de287940bf1818386d9c062955a6ced8
roiaki <roitoitoitoi@yahoo.co.jp>
2021-05-16 01:02:29 +0900

    20210516_chapter8

--- BullentinBoard/index.php
+++ BullentinBoard/index.php
@@ -1,5 +1,6 @@
 <?php
 var_dump($_POST);
+
 // メッセージを保存するファイルのパス設定
 define( 'FILENAME', './message.txt');
 
@@ -10,42 +11,72 @@ $file_handle = null;
 $split_data = null;
 $message = array();
 $message_array = array();
+$success_message = null;
+$error_message = array();
+$clean = array();
+
 
 // タイムゾーン設定
 date_default_timezone_set('Asia/Tokyo');
 
+// 書き込みボタンが押されたら
 if( !empty($_POST['btn_submit']) ) {
-	
-	// 「w」はファイル内容を一旦リセットして書き込みを行い、
-	// 「a」は末端から追記する形で書き込み
-	// ファイルが開けたら　true
-	if( $file_handle = fopen( FILENAME, "a") ) {
-	
-		// 書き込み日時を取得
-		$now_date = date("Y-m-d H:i:s");
-	
-		// 書き込むデータを作成
-		$data = "'".$_POST['view_name']."','".$_POST['message']."','".$now_date."'\n";
-			
-		// 書き込み
-		fwrite( $file_handle, $data);
 
-		// ファイルを閉じる
-		fclose( $file_handle);
-	}	
+	// 表示名入力チェックバリデーション
+	if(empty($_POST['view_name'])) {
+		$error_message[] ='表示名を入力してください。';
+	} else {
+		$clean['view_name'] = htmlspecialchars($_POST['view_name'], ENT_QUOTES);
+		$clean['view_name'] = preg_replace( '/\\r\\n|\\n|\\r/', '', $clean['view_name']);
+	}
+
+	// メッセージの入力チェック
+	if( empty($_POST['message']) ) {
+		$error_message[] = 'ひと言メッセージを入力してください。';
+	} else {
+		$clean['message'] = htmlspecialchars($_POST['message'], ENT_QUOTES);
+		$clean['message'] = preg_replace( '/\\r\\n|\\n|\\r/', '<br>', $clean['message']);
+	}
+
+	// バリデーションエラーがなければファイル書き込みOK
+	if(empty($error_message)) {
+		
+		// 「w」はファイル内容を一旦リセットして書き込みを行い、
+		// 「a」は末端から追記する形で書き込み
+		// ファイルが開けたら　true
+		if( $file_handle = fopen( FILENAME, "a") ) {
+			
+			// 書き込み日時を取得
+			$now_date = date("Y-m-d H:i:s");
+		
+			// 書き込むデータを作成
+			$data = "'" . $clean['view_name'] . "','" . $clean['message'] . "','" . $now_date . "'\n";
+				
+			// 書き込み
+			fwrite( $file_handle, $data);
+
+			// ファイルを閉じる
+			fclose( $file_handle);
+
+			$success_message = 'メッセージを書き込みました。';
+		}	
+	}
 }
 
 // [r]:読み込みのみでオープン
 if( $file_handle = fopen( FILENAME,'r') ) {
     
 	while( $data = fgets($file_handle) ){
+		
+		// ??????? 区切るスラッシュの前にバックスラッシュ,「‘」を文字として検索するために必要な記号 ???????
         $split_data = preg_split( '/\'/', $data);
-
+		
         $message = array(
             'view_name' => $split_data[1],
             'message' => $split_data[3],
             'post_date' => $split_data[5]
         );
+		// array の先頭に指定された要素を加えます
         array_unshift( $message_array, $message);
     }
     // ファイルを閉じる
@@ -65,6 +96,19 @@ if( $file_handle = fopen( FILENAME,'r') ) {
 <body>
 <h1>ひと言掲示板</h1>
 <!-- ここにメッセージの入力フォームを設置 -->
+
+<?php if(!empty($success_message)) : ?>
+	<p class="success_message"><?php echo $success_message; ?></p>
+<?php endif; ?>
+
+<?php if( !empty($error_message) ): ?>
+	<ul class="error_message">
+		<?php foreach( $error_message as $value ): ?>
+			<li>・<?php echo $value; ?></li>
+		<?php endforeach; ?>
+	</ul>
+<?php endif; ?>
+
 <form method="post">
 	<div>
 		<label for="view_name">表示名</label>
@@ -82,6 +126,7 @@ if( $file_handle = fopen( FILENAME,'r') ) {
 
 <?php if( !empty($message_array) ): ?>
 <?php foreach( $message_array as $value ): ?>
+<!-- article:内容が単体で完結するセクションである -->
 <article>
     <div class="info">
         <h2><?php echo $value['view_name']; ?></h2>
@@ -89,6 +134,7 @@ if( $file_handle = fopen( FILENAME,'r') ) {
     </div>
     <p><?php echo $value['message']; ?></p>
 </article>
+
 <?php endforeach; ?>
 <?php endif; ?>
 </section>
